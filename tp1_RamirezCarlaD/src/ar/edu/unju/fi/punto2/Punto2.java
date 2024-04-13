b9de1af9c47119ba06759140105adaba2058f342
Carla <ramirezcarla2809@gmail.com>
2024-04-13 10:22:58 -0300

    Commit del punto 2 del Trabajo Practico

---
+++ tp1_RamirezCarlaD/src/ar/edu/unju/fi/punto2/Punto2.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.punto2;
+
+public class Punto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String pais = "Argentina";
+		int edad = 23;
+		float altedif = (float) 20.08; //altura del edificio
+		double precio = 15.000; //precio de un producto
+		int telefono = 388593258;
+		float coseno = (float) 0.8;
+		
+		System.out.println("Pais: " + pais);
+		System.out.println("Edad: " + edad);
+		System.out.println("Altura del edificio: " + altedif);
+		System.out.println("Precio: $" + precio);
+		System.out.println("Telefono: " + telefono);
+		System.out.println("El coseno de 0.5: " + coseno);
+		
+		
+	}
+
+}
