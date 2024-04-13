6d4a347602f2c81f459089ea7ebfa97d2e56a071
Carla <ramirezcarla2809@gmail.com>
2024-04-13 10:35:02 -0300

    correccion del item calcular coseno de 0.5

--- tp1_RamirezCarlaD/src/ar/edu/unju/fi/punto2/Punto2.java
+++ tp1_RamirezCarlaD/src/ar/edu/unju/fi/punto2/Punto2.java
@@ -10,15 +10,14 @@ public class Punto2 {
 		float altedif = (float) 20.08; //altura del edificio
 		double precio = 15.000; //precio de un producto
 		int telefono = 388593258;
-		float coseno = (float) 0.8;
+		float coseno = (float) 0.5;
 		
 		System.out.println("Pais: " + pais);
 		System.out.println("Edad: " + edad);
 		System.out.println("Altura del edificio: " + altedif);
 		System.out.println("Precio: $" + precio);
 		System.out.println("Telefono: " + telefono);
-		System.out.println("El coseno de 0.5: " + coseno);
-		
+		System.out.println("El coseno de 0.5: " + Math.cos(coseno));
 		
 	}
 
