671ab1498aa617b74e25b3ce44284cf911f90aa9
Carla <ramirezcarla2809@gmail.com>
2024-05-07 00:32:32 -0300

    Punto 8 terminado

---
+++ tp1_RamirezCarlaD/src/ar/edu/unju/fi/punto8/main/Main.java
@@ -0,0 +1,24 @@
+package ar.edu.unju.fi.punto8.main;
+
+import java.util.Scanner;
+import ar.edu.unju.fi.punto8.model.CalculadoraEspecial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero;
+		Scanner leer = new Scanner(System.in);
+
+		CalculadoraEspecial calculadora = new CalculadoraEspecial();
+
+		System.out.print("Ingrese número: ");
+		numero = leer.nextInt();
+
+		calculadora.setN(numero);
+		calculadora.calcularSumatoria();
+		calculadora.calcularProductoria();
+
+	}
+
+}
